435e21ac847cd9382d07fb772594c58cb731556b
mbeka02 <antonymbeka@gmail.com>
2025-02-12 22:32:18 +0300

    fix: axios setup

--- services/api.ts
+++ services/api.ts
@@ -14,9 +14,7 @@ import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
 import { TOKEN_KEY } from "~/constants";
 const url = process.env.EXPO_PUBLIC_DEV_URL;
 
-// const url = "https://4c51-2001-8a0-fa2b-2a01-b961-fd91-6666-abb2.ngrok-free.app"
-
-const Api: AxiosInstance = axios.create({ baseURL: url + "/api" });
+const Api: AxiosInstance = axios.create({ baseURL: url });
 
 Api.interceptors.request.use(async (config) => {
   const data = await SecureStore.getItemAsync(TOKEN_KEY);
