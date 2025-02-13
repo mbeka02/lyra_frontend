d05ce808d646f13fe40a0db3fb6e1281c634d595
mbeka02 <antonymbeka@gmail.com>
2025-02-13 12:38:18 +0300

    updated User type

--- types/index.ts
+++ types/index.ts
@@ -3,14 +3,10 @@ export enum ModalType {
   SignUp = "signup",
 }
 
-type ProfileImage = {
-  String: string;
-  Valid: boolean;
-};
 export type User = {
   full_name: string;
   email: string;
   telephone_number: string;
   role: string;
-  profile_image_url: ProfileImage;
+  profile_image_url: string;
 };
