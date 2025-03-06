e3d731193bdd617b7727012d2519952f8de7a1e7
mbeka02 <antonymbeka@gmail.com>
2025-03-06 12:28:27 +0300

    updated forms

--- app/(protected)/onboarding.tsx
+++ app/(protected)/onboarding.tsx
@@ -9,14 +9,14 @@ import { Logout } from "~/components/auth/Logout";
 import { useState } from "react";
 function OnBoardingScreen() {
   const { authState } = useAuthentication();
-  const [selectedIndex, setSelectedIndex] = useState(1);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <SafeAreaView className="py-4 px-6 mt-12">
-      <View className="flex-row justify-end mx-6 my-4 p-1 ">
+      <View className="flex-row justify-end mx-2 mt-2 mb-4 p-1 ">
         <Logout />
       </View>
-      <View className="mt-8 mb-4">
+      <View className="my-8">
         <Text className="text-2xl font-jakarta-semibold">
           Welcome {authState?.user?.full_name} 👋
         </Text>
--- components/onboarding/PatientForm.tsx
+++ components/onboarding/PatientForm.tsx
@@ -1,7 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import { Controller, useForm } from "react-hook-form";
-import { Pressable, PressableProps, View } from "react-native";
+import { useForm } from "react-hook-form";
+import { View } from "react-native";
 import { Text } from "../ui/text";
 import { Button, ButtonProps } from "../ui/button";
 import FormInput from "../form/FormInput";
@@ -20,12 +20,13 @@ import Animated, {
   LinearTransition,
 } from "react-native-reanimated";
 import { Pagination } from "./Pagination";
+
 const AnimatedButton = Animated.createAnimatedComponent(Button);
-// const _spacing=8;
-// const _buttonHeight=42;
+
 const _layoutTransition = LinearTransition.springify()
   .damping(80)
   .stiffness(200);
+
 function FormButton({
   children,
   className,
@@ -43,12 +44,15 @@ function FormButton({
     </AnimatedButton>
   );
 }
+
 type FormData = z.infer<typeof patientOnboardingSchema>;
+
 interface PatientFormProps {
   selectedIndex: number;
   total: number;
   onIndexChange: (index: number) => void;
 }
+
 export function PatientForm({
   selectedIndex,
   total,
@@ -56,7 +60,7 @@ export function PatientForm({
 }: PatientFormProps) {
   const router = useRouter();
   const { authState } = useAuthentication();
-  const { control, handleSubmit } = useForm({
+  const { control, handleSubmit } = useForm<FormData>({
     defaultValues: {
       allergies: "",
       past_medical_history: "",
@@ -64,74 +68,113 @@ export function PatientForm({
       current_medication: "",
       insurance_provider: "",
       insurance_policy_number: "",
+      address: "",
+      emergency_contact_name: "",
+      emergency_contact_phone: "",
     },
     resolver: zodResolver(patientOnboardingSchema),
   });
+
   const onSubmit = async (data: FormData) => {
     try {
       await onboardPatient(data);
       await completeOnboarding(authState?.user?.email!);
-      toast.success("completed onboarding");
+      toast.success("Completed onboarding");
       router.replace("/(protected)/(tabs)/home");
     } catch (error) {
       console.error(error);
-      toast.error("error:unable to complete onboarding");
+      toast.error("Error: Unable to complete onboarding");
     }
   };
-  return (
-    <View className="px-2 mt-1">
-      <Pagination total={total} selectedIndex={selectedIndex} />
-      <FormInput
-        name="allergies"
-        title="Allergies (optional)"
-        control={control}
-        placeholder="enter any allergies that you may have"
-      />
-      <FormInput
-        name="past_medical_history"
-        title="Past Medical History (optional)"
-        control={control}
-        placeholder=""
-        className="h-36"
-      />
-      <FormInput
-        name="family_medical_history"
-        title="Family Medical History (optional)"
-        control={control}
-        placeholder=""
-        className="h-36"
-      />
-      <FormInput
-        name="current_medication"
-        title="Current Medication (optional)"
-        control={control}
-        placeholder=""
-      />
-      <FormInput
-        name="insurance_provider"
-        title="Insurance Provider(optional)"
-        control={control}
-        placeholder=""
-      />
 
-      <FormInput
-        name="insurance_policy_number"
-        title="Insurance Policy Number (optional)"
-        control={control}
-      />
-      <Button
-        className="bg-greenPrimary font-jakarta-semibold   py-2 px-1  my-8 rounded-lg"
-        onPress={handleSubmit(onSubmit)}
-      >
-        <Text className="text-white font-jakarta-semibold ">Submit</Text>
-      </Button>
-      <View className="flex-row items-center gap-2">
+  return (
+    <View className="px-2 mt-1  h-[65%] relative">
+      <Pagination total={total} selectedIndex={selectedIndex} />
+
+      {selectedIndex === 0 && (
+        <>
+          <Text className="font-jakarta-semibold text-2xl my-1">
+            Personal Information
+          </Text>
+          <FormInput
+            name="address"
+            title="Address (optional)"
+            control={control}
+            placeholder="e.g Buruburu phase 5"
+          />
+          <FormInput
+            name="emergency_contact_name"
+            title="Emergency Contact Name (optional)"
+            control={control}
+            placeholder="Enter the name of your guardian"
+          />
+          <FormInput
+            name="emergency_contact_phone"
+            title="Emergency Contact Phone (optional)"
+            control={control}
+            placeholder="e.g. +254115826546"
+          />
+        </>
+      )}
+
+      {selectedIndex === 1 && (
+        <>
+          <Text className="font-jakarta-semibold text-2xl my-1">
+            Medical Information
+          </Text>
+          <FormInput
+            name="allergies"
+            title="Allergies (optional)"
+            control={control}
+            placeholder="e.g pollen, nuts"
+          />
+          <FormInput
+            name="past_medical_history"
+            title="Past Medical History (optional)"
+            control={control}
+            placeholder="e.g Asthma diagnosis in childhood"
+            className="h-36"
+          />
+          <FormInput
+            name="family_medical_history"
+            title="Family Medical History (if relevant)"
+            control={control}
+            placeholder="e.g Cancer"
+            className="h-36"
+          />
+          <FormInput
+            name="current_medication"
+            title="Current Medication (optional)"
+            control={control}
+            placeholder="e.g. Ibuprofen 200mg"
+          />
+        </>
+      )}
+
+      {selectedIndex === 2 && (
+        <>
+          <Text className="font-jakarta-semibold text-2xl my-1">
+            Insurance Information
+          </Text>
+          <FormInput
+            name="insurance_provider"
+            title="Insurance Provider (optional)"
+            control={control}
+            placeholder="e.g. Old Mutual"
+          />
+          <FormInput
+            name="insurance_policy_number"
+            title="Insurance Policy Number (optional)"
+            control={control}
+          />
+        </>
+      )}
+
+      <View className="flex-row items-center absolute bottom-0 gap-2">
         {selectedIndex > 0 && (
           <FormButton
-            className="dark:bg-white bg-slate-100  flex-1"
-            onPress={() => {
-              onIndexChange(selectedIndex - 1);
-            }}
+            className="dark:bg-white bg-slate-100 flex-1"
+            onPress={() => onIndexChange(selectedIndex - 1)}
           >
             <Text className="font-jakarta-semibold text-black">Back</Text>
           </FormButton>
@@ -139,18 +182,21 @@ export function PatientForm({
         <FormButton
           className="bg-greenPrimary flex-1"
           onPress={() => {
-            if (selectedIndex >= total - 1) return;
+            if (selectedIndex >= total - 1) {
+              handleSubmit(onSubmit)();
+              return;
+            }
             onIndexChange(selectedIndex + 1);
           }}
         >
           {selectedIndex === total - 1 ? (
             <Animated.Text
-              key="finish"
+              key="submit"
               className="font-jakarta-semibold text-white"
               entering={FadeInDown.springify().damping(80).stiffness(200)}
               exiting={FadeOutUp.springify().damping(80).stiffness(200)}
             >
-              Finish
+              Submit
             </Animated.Text>
           ) : (
             <Animated.Text
