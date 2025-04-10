0c74b007de00b9cd166d49e46a1707cdf9d395e8
mbeka02 <antonymbeka@gmail.com>
2025-04-10 15:04:55 +0300

    updated the bg color of the loader to match the current bg of the app

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -2,7 +2,7 @@ import { ActivityIndicator, View } from "react-native";
 
 export function Loader() {
   return (
-    <View className="flex-1 justify-center items-center bg-transparent ">
+    <View className="flex-1 justify-center items-center bg-background ">
       <ActivityIndicator size="large" color="#24AE7C" />
     </View>
   );
