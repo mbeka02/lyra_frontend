4e136a49152cc9bf25119f7a6e4aa545309938e6
mbeka02 <antonymbeka@gmail.com>
2025-04-26 15:57:33 +0300

    added safe area view

--- app/(protected)/(drawer)/appointments/_layout.tsx
+++ app/(protected)/(drawer)/appointments/_layout.tsx
@@ -1,22 +1,34 @@
 import { Stack } from "expo-router";
-
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Platform } from "react-native";
 export default function SettingsLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="[id]"
-        options={
-          {
-            // headerShown: false,
+    <SafeAreaView
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+      edges={Platform.select({
+        ios: ["top", "right", "left"],
+        android: ["top", "right", "left"],
+      })}
+    >
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="[id]"
+          options={
+            {
+              // headerShown: false,
+            }
           }
-        }
-      />
-    </Stack>
+        />
+      </Stack>
+    </SafeAreaView>
   );
 }
