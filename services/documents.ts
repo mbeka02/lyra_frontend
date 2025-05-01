e06280c563b081cad94a513f5b144590e64fa47d
mbeka02 <antonymbeka@gmail.com>
2025-05-01 21:24:47 +0300

    added a function that fetchespatient records

--- services/documents.ts
+++ services/documents.ts
@@ -1,5 +1,15 @@
 import { Api } from "./api";
+import type { Bundle, DocumentReference } from "~/types/fhir";
 
+// Define the structure of the Bundle returned by your API
+interface DocumentReferenceBundle extends Bundle {
+  entry?: { resource: DocumentReference }[]; // Specify the resource type in entry
+}
 export function UploadPatientDocument(data: FormData) {
   return Api.post("/documents/upload", data);
+}
+
+export function GetPatientDocuments(): Promise<DocumentReferenceBundle> {
+  //TODO: Append patient parameters
+  return Api.get("/documents");
 }
